6d7615d1ae3942bbc9d701fdeea15732bf3534c4
1akshat <akshat.akshat6@gmail.com>
2020-08-22 19:34:34 +0530

    banner-component -> done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import NavBar from "../src/components/NavBar/NavBar";
+import Home from "./components/Home/Home";
 import "./App.css";
 
 function App() {
   return (
     <Router>
       <div className="app">
-        <NavBar />
         <Switch>
           <Route path="/login"></Route>
-          <Route path="/checkout"></Route>
-          <Route path="/"></Route>
+          <Route path="/checkout">
+            <NavBar />
+          </Route>
+          <Route path="/">
+            <NavBar />
+            <Home />
+          </Route>
         </Switch>
       </div>
     </Router>
---
+++ src/components/Home/Banner/Banner.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Banner.css";
+
+const Banner = () => {
+  return (
+    <div className="banner">
+      <img
+        className="banner-image"
+        src="https://images-eu.ssl-images-amazon.com/images/G/31/img19/AmazonPay/Rajeshwari/AugOffers/GW22Aug/DesktopHero_1500x600._CB407658174_.jpg"
+        alt="banner-image"
+      />
+    </div>
+  );
+};
+
+export default Banner;
